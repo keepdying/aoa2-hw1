b626fed874afc7f34b9df1eb0fe76ed2a864373a
Emre Cakiroglu <yunusemre34tr@gmail.com>
2023-03-17 07:00:10 +0000

    init repo

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// using namespace std;
+
+int main(){
+    /*
+        INSERT ALL YOUR CODE IN HERE!
+        Program Compile Command: g++ -std=c++11 -Wall -Werror main.cpp -o main
+        Program Run Command: ./main <input.txt>
+        Expected input: /graphs/case{$n}/input_{$n}.txt
+        Expected output: graph.txt bfs.txt dfs.txt
+        Please, try to write clean and readable code. Remember to comment!!
+    */
+    exit(0);
+}
